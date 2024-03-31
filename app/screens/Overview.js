916a0d110f08dc4032258d685200650b80b558ab
shust <shust@DESKTOP-6GNUVDA>
2024-03-31 12:38:07 -0600

    moved into app folder

---
+++ app/screens/Overview.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, StyleSheet, Text } from "react-native";
+
+function Overview(props) {
+    return (
+        <View>
+            <Text style={styles.Text}></Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    Text: {
+        backgroundColor: 'blue',
+    },
+    });
+
+export default Overview;
